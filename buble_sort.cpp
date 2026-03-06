fa82afe363c5e7bb6152bfe750ebee3aa2c7dac5
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-03-06 15:26:17 +0700

    menambah void input

--- buble_sort.cpp
+++ buble_sort.cpp
@@ -3,3 +3,32 @@ using namespace std;
 
 int a[20];
 int n;
+
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan panjang array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Error : Maksimal input array adalah 20\n";
+        }
+    }
+    
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen Array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Masukkan Nilai ke - " << i + i <, " : ";
+        cin >> a[i];
+    }
+}
